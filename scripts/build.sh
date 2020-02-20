86e7f39335b0eab002a7a6beb64cef08a14974af
channingss <chen_lingchi@163.com>
2020-02-20 03:37:53 +0000

    fix bug in compile

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 cmake .. \
     -DWITH_GPU=ON \
-    -DPADDLE_DIR=/root/projects/infer_lib/fluid_inference/ \
+    -DPADDLE_DIR=/docker/deploy-Dec/fluid_inference/ \
     -DCUDA_LIB=/usr/local/cuda/lib64/ \
-    -DOPENCV_DIR=/root/projects/opencv3gcc4.8/ \
-    -DCUDNN_LIB=/usr/local/cuda/lib64/ \
+    -DOPENCV_DIR=/root/projects/opencv3/ \
+    -DCUDNN_LIB=/usr/lib/x86_64-linux-gnu/ \
     -DWITH_STATIC_LIB=OFF
 make clean
 make -j12
