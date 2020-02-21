e5b9c050863632006b065cf039c0efe7f43844b5
channingss <chen_lingchi@163.com>
2020-02-21 10:45:38 +0800

    delete some file

--- include/paddle_cv_inference.h
+++
@@ -1,6 +0,0 @@
-
-# include <object_detection/object_detection.h>
-# include <object_segmentation/object_segmentation.h>
-# include <object_classification/object_classification.h>
-# include <utils/utils.h>
-
