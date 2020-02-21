26dca75d3e480149381ce2698580b4f4319ca6fa
channingss <chen_lingchi@163.com>
2020-02-21 17:11:04 +0800

    fix bug

--- linux_build.sh
+++ linux_build.sh
@@ -1,23 +1,21 @@
-WITH_MKL=ON
+
 WITH_GPU=ON
 
-PADDLE_DIR=/home/chenlingchi/fluid_inference/
+PADDLE_DIR=/root/projects/infer_lib/fluid_inference/
 CUDA_LIB=/usr/local/cuda/lib64/
 CUDNN_LIB=/usr/local/cuda/lib64/
-OPENCV_DIR=/home/chenlingchi/project/opencv3-g48/
+OPENCV_DIR=/root/projects/opencv3gcc4.8/
 
 rm -rf build
 mkdir -p build
 cd build
 
 cmake .. \
-    -DWITH_MKL=${WITH_MKL} \
     -DWITH_GPU=${WITH_GPU} \
     -DPADDLE_DIR=${PADDLE_DIR} \
     -DCUDA_LIB=${CUDA_LIB} \
     -DCUDNN_LIB=${CUDNN_LIB} \
     -DOPENCV_DIR=${OPENCV_DIR} \
     -DWITH_STATIC_LIB=OFF
-
 make clean
 make -j12
