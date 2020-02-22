911cdf508480bdec509fd833fb1c04c7a5ddd318
sjtubinlong <binlong.plus@gmail.com>
2020-02-22 11:21:31 +0800

    update project structure

--- mask_detector.cc
+++ mask_detector.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-# include "mask_detector.hpp"
+# include "mask_detector.h"
 
 // Normalize the image by (pix - mean) * scale
 void NormalizeImage(
